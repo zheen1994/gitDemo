f734ae53bcc09d7643d2dd0e916532afa9e1f6cf
Heen <1071275594@qq.com>
2020-07-07 17:04:13 +0800

    eclipse提交

--- src/pushGitProject/HelloGit.java
+++ src/pushGitProject/HelloGit.java
@@ -3,7 +3,7 @@ package pushGitProject;
 public class HelloGit {
 
 	public static void main(String args[]){
-        System.out.println("Hi22266666622366Git");
+        System.out.println("HiEclipseGit");
 
     }
 }
