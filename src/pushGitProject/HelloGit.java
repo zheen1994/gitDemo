1e7ffec99886e03742c63d38d0e19e7ddb2ec50a
Heen <1071275594@qq.com>
2020-07-07 17:25:32 +0800

    eclipse第三次修改

--- src/pushGitProject/HelloGit.java
+++ src/pushGitProject/HelloGit.java
@@ -3,7 +3,7 @@ package pushGitProject;
 public class HelloGit {
 
 	public static void main(String args[]){
-        System.out.println("HiEclipseGit2");
+        System.out.println("HiEclipseGit3");
         System.out.println("HiIdeaGit");
 
     }
